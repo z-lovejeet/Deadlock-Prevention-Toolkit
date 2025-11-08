ce45052e3e8582e4212dbdd361fdfd958f34caff
z-lovejeet <lovejeet1225@gmail.com>
2025-11-08 14:01:15 +0530

    Initial commit - added all project files

---
+++ algorithms/banker.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Banker's Algorithm Example" << endl;
+    // Implementation of Banker's Algorithm would go here
+    return 0;
+}
